41f7c1612f12f0a2c3534cbbb78a1ba24c62c762
Sanjeet kumar <sanjeetkum960@gmail.com>
2025-01-28 23:06:38 +0530

    pattern 4

---
+++ pattern4.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=1;i<=6;i++){
+        for(int j=0;j<6-i+1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+}
