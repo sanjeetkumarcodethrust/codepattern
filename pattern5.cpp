ff83580d6ceb3d80d04cfdba740a7c456e5485ee
Sanjeet kumar <sanjeetkum960@gmail.com>
2025-01-28 23:13:22 +0530

    pattern 5

---
+++ pattern5.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=1;i<=6;i++){
+        for(int j=1;j<6-i+1;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
